46f7e12f2b167f6776528bc7d611bd406b2a1288
Falco Prescher <falcoprescher@googlemail.com>
2015-05-08 07:59:14 +0200

    Hinzufügen des Teambereichs

--- js/main.js
+++ js/main.js
@@ -2,15 +2,18 @@ $(function() {
 	$('#expertsTable').dataTable({
 		'ajaxSource': 'js/experts.json',
 		'columns': [
-			{ 'data': 'image', 'title': 'Portrait', 'class': 'expertPortrait' },
-			{ 'data': 'name', 'title': 'Name' },
-			{ 'data': 'city', 'title': 'Stadt' },
-			{ 'data': 'department', 'title': 'Hauptgruppe' },
-			{ 'data': 'phone', 'title': 'Telefon' }],
+			{ 'data': 'image', 'title': 'Portrait', 'class': 'colPortrait' },
+			{ 'data': 'name', 'title': 'Name', 'class': 'colName' },
+			{ 'data': 'city', 'title': 'Stadt', 'class': 'colStadt' },
+			{ 'data': 'department', 'title': 'Hauptgruppe', 'class': 'colHauptgruppe' },
+			{ 'data': 'phone', 'title': 'Telefon', 'class': 'colTelefon' }],
+		// Doent calculate on first init, instead use class widths
+		"bAutoWidth": false,
+		// Templating for the first column
 		'columnDefs': [ {
 	    'targets': 0,
 	    'data': 'image',
-	    'render': function ( data, type, full, meta ) {
+	    'render': function ( data ) {
 	      	return '<img src="' + data + '" alt="" />';
 	    }
 	  } ]
